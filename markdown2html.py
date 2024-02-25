a8742cc29b99406efae5e407bb161f3a0f2ebf2e
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 21:10:34 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-""" 
-Write a script markdown2html.py that takes an argument 2 strings:
-First argument is the name of the Markdown file
-Second argument is the output file name 
-"""
-
-import os
+''' Markdown to HTML '''
 import sys
-import markdown2html
-
-
-def convert_markdown_to_html(input_file):
-    """Convert Markdown to HTML"""
-    with open(input_file, 'r') as md_file:
-        markdown_content = md_file.read()
-        html_content = markdown2html.markdown(markdown_content)
-    return html_content
+from os import path
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    '''the number of arguments is less than 2'''
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
         exit(1)
-
-    input_file = args[1]
-    output_file = args[2]
-
-    if not os.path.exists(input_file):
-        sys.stderr.write(f"Missing {input_file}\n")
+    '''Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
-
-    # Convert Markdown to HTML
-    html_content = convert_markdown_to_html(input_file)
-
-    # Write HTML content to the output file
-    with open(output_file, 'w') as html_file:
-        html_file.write(html_content)
-
-    exit(0)
+    sys.exit(0)
