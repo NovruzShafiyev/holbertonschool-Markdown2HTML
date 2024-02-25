3113ecebb1905a1156c05151c9090ea2d954970d
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 19:53:58 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -1,33 +1,39 @@
+#!/usr/bin/python3
+
 import sys
 import os
 import markdown2html
 
 def main():
-  # Check command line arguments
-  if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py <input_file.md> <output_file.html>", file=sys.stderr)
-    sys.exit(1)
+    """Converts a Markdown file to HTML.
 
-  # Check if input file exists
-  input_file = sys.argv[1]
-  if not os.path.exists(input_file):
-    print(f"Missing {input_file}", file=sys.stderr)
-    sys.exit(1)
+    Args:
+        sys.argv: List of command-line arguments.
 
-  # Convert markdown to html
-  with open(input_file, 'r') as file:
-    markdown_text = file.read()
-  html_content = markdown2html.markdown(markdown_text)
+    Returns:
+        None. Exits with error code 0 on success, 1 otherwise.
+    """
 
-  # Write html to output file
-  output_file = sys.argv[2]
-  with open(output_file, 'w') as file:
-    file.write(html_content)
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <input_file.md> <output_file.html>", file=sys.stderr)
+        sys.exit(1)
 
-  # Success exit
-  sys.exit(0)
+    input_file = sys.argv[1]
+    if not os.path.exists(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    with open(input_file, 'r') as file:
+        markdown_text = file.read()
+
+    html_content = markdown2html.markdown(markdown_text)
+
+    output_file = sys.argv[2]
+    with open(output_file, 'w') as file:
+        file.write(html_content)
+
+    # Success
+    sys.exit(0)
 
 if __name__ == "__main__":
-  main()
-
-
+    main()
