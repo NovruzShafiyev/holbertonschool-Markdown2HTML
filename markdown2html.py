fe16c7dad77fbaf8574a612413c683bbb04e672f
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 21:15:07 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -7,10 +7,13 @@ from os import path
 if __name__ == "__main__":
     '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
-        exit(1)
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        sys.exit(1)
     '''Markdown file doesn’t exist'''
     if not path.exists(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        exit(1)
+        sys.exit(1)
     sys.exit(0)
+
+
+
