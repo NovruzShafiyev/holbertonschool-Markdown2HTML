5c0fc2900ac29e1b3386d9bb35c11eebbdbdeb60
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 19:47:14 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,33 @@
-
-import os
 import sys
+import os
 import markdown2html
 
-if len(sys.argv) < 3:
+def main():
+  # Check command line arguments
+  if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py <input_file.md> <output_file.html>", file=sys.stderr)
     sys.exit(1)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-
-if not os.path.exists(input_file):
+  # Check if input file exists
+  input_file = sys.argv[1]
+  if not os.path.exists(input_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
 
-with open(input_file, 'r') as file:
+  # Convert markdown to html
+  with open(input_file, 'r') as file:
     markdown_text = file.read()
+  html_content = markdown2html.markdown(markdown_text)
 
-html_content = markdown2html.markdown(markdown_text)
-
-with open(output_file, 'w') as file:
+  # Write html to output file
+  output_file = sys.argv[2]
+  with open(output_file, 'w') as file:
     file.write(html_content)
 
-sys.exit(0)
+  # Success exit
+  sys.exit(0)
+
+if __name__ == "__main__":
+  main()
+
 
