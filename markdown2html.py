84f22f2a681c00e08421136d2e621d6e3db8ae76
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 21:25:33 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -4,6 +4,17 @@ import sys
 from os import path
 
 
+def convert_heading(line):
+    ''' Convert Markdown heading to HTML '''
+    if line.startswith('#'):
+        count = line.count('#')
+        tag = 'h' + str(count)
+        text = line.strip('#').strip()
+        return f'<{tag}>{text}</{tag}>\n'
+    else:
+        return line
+
+
 if __name__ == "__main__":
     '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
@@ -13,7 +24,13 @@ if __name__ == "__main__":
     if not path.exists(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
+
+    with open(sys.argv[1], 'r') as markdown_file:
+        markdown_lines = markdown_file.readlines()
+
+    html_lines = [convert_heading(line) for line in markdown_lines]
+
+    with open(sys.argv[2], 'w') as html_file:
+        html_file.writelines(html_lines)
+
     sys.exit(0)
-
-
-
