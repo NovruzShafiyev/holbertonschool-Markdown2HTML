e896e4ad00fc6c8283d4815e837fe62c3ee85643
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 21:53:32 +0400

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # holbertonschool-Markdown2HTML
+- Hello
+- Bye
--- markdown2html.py
+++ markdown2html.py
@@ -15,6 +15,28 @@ def convert_heading(line):
         return line
 
 
+def convert_unordered_list(markdown_lines):
+    ''' Convert Markdown unordered list to HTML '''
+    html_lines = []
+    in_list = False
+    for line in markdown_lines:
+        line = line.strip()
+        if line.startswith('-'):
+            if not in_list:
+                html_lines.append('<ul>\n')
+                in_list = True
+            line = line.lstrip('-').strip()
+            html_lines.append(f'    <li>{line}</li>\n')
+        else:
+            if in_list:
+                html_lines.append('</ul>\n')
+                in_list = False
+            html_lines.append(line)
+    if in_list:
+        html_lines.append('</ul>\n')
+    return html_lines
+
+
 if __name__ == "__main__":
     '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
@@ -25,11 +47,14 @@ if __name__ == "__main__":
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
 
+    # Convert Markdown to HTML
     with open(sys.argv[1], 'r') as markdown_file:
         markdown_lines = markdown_file.readlines()
 
-    html_lines = [convert_heading(line) for line in markdown_lines]
+    html_lines_heading = [convert_heading(line) for line in markdown_lines]
+    html_lines = convert_unordered_list(html_lines_heading)
 
+    # Write HTML to output file
     with open(sys.argv[2], 'w') as html_file:
         html_file.writelines(html_lines)
 
