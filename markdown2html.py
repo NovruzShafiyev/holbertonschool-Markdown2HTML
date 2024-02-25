e66a9d852031d9b2f7f686a4c5a1600bc3aa9f8e
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-02-25 20:58:00 +0400

    done

--- markdown2html.py
+++ markdown2html.py
@@ -1,27 +1,26 @@
-#!/usr/bin/python3
-""" 
-Write a script markdown2html.py that takes an argument 2 strings:
-First argument is the name of the Markdown file
-Second argument is the output file name 
-"""
-
 import os
 import sys
 
-
-
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
+
+    if len(args) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py <input_file.md> <output_file.html>\n")
+        sys.exit(1)
 
     input_file = args[1]
     output_file = args[2]
 
     if not os.path.exists(input_file):
         sys.stderr.write(f"Missing {input_file}\n")
-        exit(1)
+        sys.exit(1)
 
+    # Add the code for actual Markdown to HTML conversion here
+    # For example, using the markdown library:
+    import markdown
 
-    exit(0)
+    with open(input_file, 'r') as md_file, open(output_file, 'w') as html_file:
+        html_content = markdown.markdown(md_file.read())
+        html_file.write(html_content)
+
+    sys.exit(0)
